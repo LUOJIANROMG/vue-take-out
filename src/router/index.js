b784de42fbd490ba070d5166f0dc5c604b767fcd
LUOJIANRONG <834724495@qq.com>
2018-09-12 21:15:21 +0800

    init commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+/*路由器对象模块*/
+import  Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import MSite from '../pages/MSite/Msite.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Search from '../pages/Search/Search.vue'
+
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  //所有路由
+  routes:[
+    {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
